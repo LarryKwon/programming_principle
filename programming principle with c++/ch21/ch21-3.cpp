9f3158f5f5a15c04b14e31af2c00f04cdad140e4
koallarry <>
2021-10-11 13:20:42 +0000

    ch21-3 시작

---
+++ programming principle with c++/ch21/ch21-3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+#include<algorithm>
+#include<stdexcept>
+#include<memory>
+#include<vector>
+#include<list>
+#include<map>
+#include<iomanip>
+#include<numeric>
+
+using namespace std;
+
+
+struct Range_error : out_of_range {	// enhanced vector range error reporting
+	int index;
+	Range_error(int i) :out_of_range("Range error: "+to_string(i)), index(i) { }
+};
+
+
+inline void error(const string& s)
+{
+	throw runtime_error(s);
+}
+
+inline void error(const string& s, const string& s2)
+{
+	error(s+s2);
+}
+
+
+int main()
+try {
+  
+}
+catch (Range_error& re) {
+    cerr << "bad index: " << re.index << "\n";
+}
+catch (exception& e) {
+    cerr << "exception: " << e.what() << endl;
+}
+catch (...) {
+    cerr << "exception\n";
+}
